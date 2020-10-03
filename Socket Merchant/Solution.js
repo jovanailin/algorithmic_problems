2c0341dc9f84f9b7e67f369133ac5e5f08c8b7ee
Jovana Ilin <jovanailin@gmail.com>
2020-10-03 20:22:14 +0200

    added comment

--- Socket Merchant/Solution.js
+++ Socket Merchant/Solution.js
@@ -2,7 +2,7 @@ function sockMerchant(n, ar) {
     let num = 0
     let result = { };
     for(let i = 0; i < ar.length; ++i) {
-    if(!result[ar[i]])
+    if(!result[ar[i]]) // ako vec ne postoji taj property postavi mu vrednost na nulu
         result[ar[i]] = 0;
     ++result[ar[i]];
 }
